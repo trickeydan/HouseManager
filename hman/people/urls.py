0bf167b1b9081da633c26208f81d5b399163143d
Dan Trickey <dan@trickey.io>
2018-06-21 21:01:41 +0100

    Add Person Profile Page

--- hman/people/urls.py
+++ hman/people/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from .views import PeopleListView
+from .views import PeopleListView, PersonView
 
 
 app_name = 'people'
 
 urlpatterns = [
     path('', PeopleListView.as_view(), name='index'),
+    path('<int:pk>/', PersonView.as_view(), name='view')
 ]
--- hman/people/views.py
+++ hman/people/views.py
@@ -1,10 +1,16 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, DetailView
 from .models import Person
 
 
 class PeopleListView(ListView):
 
-    template_name = 'people/list.html'
+    template_name = 'people/index.html'
 
     def get_queryset(self):
         return Person.objects.all()
+
+
+class PersonView(DetailView):
+    template_name = 'people/view.html'
+    model = Person
+
